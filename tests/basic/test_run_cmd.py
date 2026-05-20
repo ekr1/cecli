10b539335ff491bc76f99d3d4980c8dfbcdc18e8
Your Name <you@example.com>
2026-05-20 04:39:10 -0400

    Pre-commit update

--- tests/basic/test_run_cmd.py
+++ tests/basic/test_run_cmd.py
@@ -4,7 +4,7 @@ from cecli.run_cmd import run_cmd
 
 
 def test_run_cmd_echo():
-    command = "echo \"Hello World\""
+    command = 'echo "Hello World"'
     exit_code, output = run_cmd(command)
 
     assert exit_code == 0
